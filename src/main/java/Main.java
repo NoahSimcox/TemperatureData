8da8fff1ea3f4040c336c57bcbe649a5424d4cc8
NoahSimcox <nssimcox@gmail.com>
2023-06-01 12:19:33 -0400

    work was done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,31 @@
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialNativeInterface;
+import jssc.SerialPortList;
+
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws SerialPortException, InterruptedException {
+    public static void main(String[] args) throws SerialPortException{
 
-        SerialPort serialPort = new SerialPort("COM3");
+        System.out.println("test");
+        System.out.println(SerialPortList.getPortNames().length);
+
+
+        SerialPort serialPort = new SerialPort(SerialPortList.getPortNames()[0]);
         serialPort.openPort();//Open serial port
-        serialPort.setParams(4800, 8, 1, 0);//Set params.
+        serialPort.setParams(4800, 8, 1, 0);//Set params
+
         int i = 0;
-        while(i < 10) {
-            int[] buffer = serialPort.readIntArray(10);
-            if(buffer!=null) {
-                for(int b:buffer) {
+        while (i < 10) {
+            byte[] buffer = serialPort.readBytes(10);
+            if (buffer != null) {
+                for (byte b : buffer) {
                     System.out.print(b + "   ");
                 }
             }
-//            i++;
-//            Thread.sleep(1000);
+            i++;
         }
     }
 }
